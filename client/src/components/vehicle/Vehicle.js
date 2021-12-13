ca31a96075280474266c87343d34cfbdbce035da
Abdulrasheed Lawal <lawalabdulrasheedbolaji@gmail.com>
2021-12-13 11:26:55 +0100

    update vehicle details card style

--- client/src/components/vehicle/Vehicle.js
+++ client/src/components/vehicle/Vehicle.js
@@ -4,15 +4,74 @@ import './vehicle.css'
 export default function Vehicles(props) {
     return (
       <>
-        <p className="vehicle">
-          Make: {props.vehicle.make} <br />
-          Model: {props.vehicle.model} <br />
-          Engine Capacity: {props.vehicle.engineCapacity} <br />
-          Engine Power (PS): {props.vehicle.enginePowerPS} <br />
-          Engine Power (KW): {props.vehicle.enginePowerKW} <br />
-          Fuel Type: {props.vehicle.fuelType} <br />
-          Body Type: {props.vehicle.bodyType}
-        </p>
+        <div className="vehicle">
+          <div className="vehicleWrapper">
+            <div className="vehicleTitle">
+              <p>{`${props.vehicle.make} ${props.vehicle.model}`}</p>
+            </div>
+            <div className="vehicleDetails">
+              <div className="layer">
+                <div className="element">
+                  <div className="vertical">
+                    <div>
+                      {props.vehicle.engineCapacity}
+                    </div>
+                    <div className="label">
+                      <p>Engine Capacity</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="element">
+                <div className="vertical">
+                  
+                </div>
+                <div className="vertical">
+                  <div>
+                      {props.vehicle.enginePowerPS}
+                    </div>
+                    <div className="label">
+                      Engine Power (PS)
+                    </div>
+                </div>
+                </div>                
+              </div>
+              <div className="layer">
+                <div className="element">
+                  <div className="vertical">
+                    <div>
+                        {props.vehicle.enginePowerKW}
+                    </div>
+                    <div className="label">
+                        Engine Power (KW)
+                    </div>
+                  </div>
+                </div>
+                <div className="element">
+                  <div className="vertical">
+                    <div>
+                        {props.vehicle.fuelType}
+                    </div>
+                    <div className="label">
+                      Fuel Type
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="layer">
+                <div className="element">
+                  <div className="vertical">
+                    <div>
+                      {props.vehicle.bodyType}
+                    </div>
+                    <div className="label">
+                      Body Type
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
